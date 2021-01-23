26e767a7e5223f3834248a01aa131aca44378f95
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-23 20:15:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 ["bob"]
-add_element_to_end_of_array.push("sam")
+add_element_to_end_of_array.push<<("sam")
 end
 
 def add_element_to_start_of_array(array, element)
